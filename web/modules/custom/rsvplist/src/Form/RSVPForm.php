d92aafc4d730b9a001cfea6d3d4bf7e8c2d346f8
Hilmar the Drupalviking Hallbjornsson <drupalviking@gmail.com>
2017-08-10 08:55:24 -0400

    Added email validation and Dependency Injection of the services used

--- web/modules/custom/rsvplist/src/Form/RSVPForm.php
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -8,6 +8,9 @@ namespace Drupal\rsvplist\Form;
 use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Egulias\EmailValidator\EmailValidatorInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class RSVPForm
@@ -16,6 +19,19 @@ use Drupal\Core\Form\FormStateInterface;
  * Provides a RSVP signup form
  */
 class RSVPForm extends FormBase {
+  /**
+   * The Route Match
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Email validation
+   *
+   * @var \Egulias\EmailValidator\EmailValidatorInterface
+   */
+  protected $email_validator;
 
   /**
    * {@inheritdoc}
@@ -28,7 +44,7 @@ class RSVPForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $node = \Drupal::routeMatch()->getParameter('node');
+    $node = $this->routeMatch->getParameter('node');
     //$nid = $node->nid->value;
     $nid = null;
     $form['name'] = [
@@ -57,10 +73,40 @@ class RSVPForm extends FormBase {
     return $form;
   }
 
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $value = $form_state->getValue('email');
+    if($value == !$this->email_validator->isValid($value)) {
+      $form_state->setErrorByName('email', $this->t('The email address %mail is 
+      not valid.', ['%mail' => $value]));
+    }
+  }
+
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     drupal_set_message($this->t('The form is working'));
+  }
+
+  /**
+   * RSVPForm constructor.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   * @param \Egulias\EmailValidator\EmailValidatorInterface $email_validator
+   */
+  public function __construct(RouteMatchInterface $route_match,
+                              EmailValidatorInterface $email_validator) {
+    $this->routeMatch = $route_match;
+    $this->email_validator = $email_validator;
+  }
+
+  /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   * @return static
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_route_match'),
+      $container->get('email.validator')
+    );
   }
 }
