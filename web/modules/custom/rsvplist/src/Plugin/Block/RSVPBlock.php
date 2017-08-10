6a51cfed21f010e5c0b1369c80c8e92645a2fe32
Hilmar the Drupalviking Hallbjornsson <drupalviking@gmail.com>
2017-08-10 10:35:16 -0400

    Added Block to display the Form, and Access control to only display on nodes

---
+++ web/modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\rsvplist\Plugin\Block\RSVPBlock
+ */
+namespace Drupal\rsvplist\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * Provides a 'RSVP' List Block
+ *
+ * @Block(
+ *   id = "rsvp_block",
+ *   admin_label = @Translation("RSVP Block"),
+ *   category = @Translation("Blocks")
+ * )
+ */
+class RSVPBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return \Drupal::formBuilder()->getForm('Drupal\rsvplist\Form\RSVPForm');
+  }
+
+  public function blockAccess(AccountInterface $account) {
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = \Drupal::routeMatch()->getParameter('node');
+    if($node) {
+      $nid = $node->nid->value;
+      if(is_numeric($nid)) {
+        return AccessResult::allowedIfHasPermission($account, 'view rsvplist');
+      }
+    }
+
+    return AccessResult::forbidden();
+  }
+}
